ae132b25e40538676a4edc05cdeac47a97a85e1c
FutuRx <futurxteam@gmail.com>
2026-09-18 20:18:06 +0530

    updt

--- src/pages/client/ClientDashboard.jsx
+++ src/pages/client/ClientDashboard.jsx
@@ -25,6 +25,7 @@ const ClientOverview = ({ onOpenRequestModal }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+
     const fetchServices = async () => {
       setLoading(true);
       try {
