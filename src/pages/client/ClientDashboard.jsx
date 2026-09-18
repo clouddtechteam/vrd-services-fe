dd842aa0ff0c010fd4265346173721e46937a4a6
clouddtechteam <clouddtech@gmail.com>
2026-09-18 20:24:56 +0530

    updt

--- src/pages/client/ClientDashboard.jsx
+++ src/pages/client/ClientDashboard.jsx
@@ -27,6 +27,7 @@ const ClientOverview = ({ onOpenRequestModal }) => {
   useEffect(() => {
     const fetchServices = async () => {
       setLoading(true);
+
       try {
         const res = await api.get('/services/my-services');
         if (res.data.success) {
